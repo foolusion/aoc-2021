c4b066fef0ba274d7471e97b9dd3aa648d71ab22
Andrew O'Neill <foolusion@gmail.com>
2021-12-01 21:46:01 -0800

    day 2

--- advent.lisp
+++ advent.lisp
@@ -1,6 +1,6 @@
 (defpackage :advent-2021
   (:use :cl)
-  (:export :day-1))
+  (:export :day-1 :day-2))
 (in-package :advent-2021)
 
 (defun read-file (file)
@@ -34,3 +34,37 @@
 (defun day-1 (input)
   (values (day-1-part-1 input)
 	  (day-1-part-2 input)))
+
+(defun move (list x y)
+  (if (null list)
+      (list x y)
+      (let* ((action (first list))
+	     (dir (first action))
+	     (dist (second action)))
+	(cond
+	  ((eq dir 'forward) (move (rest list) (+ x dist) y))
+	  ((eq dir 'down) (move (rest list) x (+ y dist)))
+	  ((eq dir 'up) (move (rest list) x (- y dist)))))))
+
+(defun day-2-part-1 (file)
+  (let ((input (read-file file)))
+    (apply #'* (move (partition 2 2 input) 0 0))))
+
+(defun move2 (list x y aim)
+  (if (null list)
+      (list x y)
+      (let* ((action (first list))
+	     (dir (first action))
+	     (dist (second action)))
+	(cond
+	  ((eq dir 'forward) (move2 (rest list) (+ x dist) (+ y (* aim dist)) aim))
+	  ((eq dir 'down) (move2 (rest list) x y (+ aim dist)))
+	  ((eq dir 'up) (move2 (rest list) x y (- aim dist)))))))
+
+(defun day-2-part-2 (file)
+  (let ((input (read-file file)))
+    (apply #'* (move2 (partition 2 2 input) 0 0 0))))
+
+(defun day-2 (file)
+  (values (day-2-part-1 file)
+	  (day-2-part-2 file)))
