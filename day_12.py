cdda25f0da115629a09ee47f69ac30d100e4062b
Andrew O'Neill <foolusion@gmail.com>
2021-12-12 16:00:34 -0800

    cleanup day 12

--- day_12.py
+++ day_12.py
@@ -6,25 +6,22 @@ with open('day_12.in') as f:
 		adjacency[b] = adjacency.get(b, []) + [a]
 
 small_caves = set(cave for cave in adjacency.keys() if cave.islower())
-paths = []
-states = [('start', {'start'}, ['start'], False)]
+paths = 0
+states = [('start', {'start'}, False)]
 while states:
-	current, been, path, double_visit = states.pop()
-	print(f'Step: current={current}, been={been}, path={path}, double_visit={double_visit}')
+	current, been, double_visit = states.pop()
 	if current == 'end':
-		paths.append(path)
+		paths += 1
 		continue
 	for step in adjacency[current]:
 		if step.islower():
 			if step in been:
-				if double_visit:
-					continue
-				elif step in ('start', 'end'):
+				if double_visit or step in ('start', 'end'):
 					continue
 				else:
-					states.append((step, been, path + [step], True))
+					states.append((step, been, True))
 			else:
-				states.append((step, been | {step}, path + [step], double_visit))
+				states.append((step, been | {step}, double_visit))
 		else:
-			states.append((step, been, path + [step], double_visit))
-print(len(paths))
+			states.append((step, been, double_visit))
+print(paths)
